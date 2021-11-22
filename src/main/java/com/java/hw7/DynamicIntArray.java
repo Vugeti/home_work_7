0addeca4ccbf9556316f78296a7c6b67e2668355
Vugeti <vugeti@gmail.com>
2021-11-22 18:20:33 +0200

    return true in if body

--- src/main/java/com/java/hw7/DynamicIntArray.java
+++ src/main/java/com/java/hw7/DynamicIntArray.java
@@ -36,8 +36,8 @@ public class DynamicIntArray {
             if (array[i] == element) {
                 System.arraycopy(array, 1, array, 0, array.length - 1);
                 size--;
+                return true;
             }
-            return true;
         }  return false;
     }
 
